10238336bc5d88493fe915a086955a54dfdd77b4
王攀 <wangpan7@xiaomi.com>
2020-12-17 15:40:54 +0800

    struct dependency works

--- yaml.c
+++ yaml.c
@@ -5,6 +5,15 @@
 
 #define MAX_LINE_CHAR_COUNT 200
 
+typedef struct
+{
+    char *name;
+    char *properties;
+    size_t properties_length;
+    bool private; // hosted on private pub server
+    bool valid;
+} dependency;
+
 int replace_hosted_to_pathed(const char *yaml_file_name, const char *path);
 bool prefix(const char *pre, const char *str);
 
@@ -15,6 +24,7 @@ int main(int argc, char *argv[])
 
 bool is_cur_line_dependencies_begin(const char *line);
 bool is_cur_line_other_begin(const char *line);
+void write_and_clear_dependency(FILE *file, dependency *d);
 int replace_hosted_to_pathed(const char *yaml_file_name, const char *path)
 {
     FILE *src;
@@ -35,10 +45,8 @@ int replace_hosted_to_pathed(const char *yaml_file_name, const char *path)
     char line[MAX_LINE_CHAR_COUNT];
 
     bool is_cur_line_in_dependency = false; // 当前处理的行是不是 dependency 中的一行
-    char *depency = NULL;                   // dependency 的名字
-    char *depency_detail = NULL;            // dependency 的属性
-    size_t detail_length = 0;               // dependency 的属性的字符串大小
-    bool is_cur_denpency_private = false;
+
+    dependency cur_dependency = {NULL, NULL, 0, false, false};
 
     while (fgets(line, MAX_LINE_CHAR_COUNT, src))
     {
@@ -59,13 +67,10 @@ int replace_hosted_to_pathed(const char *yaml_file_name, const char *path)
 
         if (!is_cur_line_in_dependency || is_cur_line_dependencies_begin(line))
         {
-            if (is_cur_line_other_begin(line) && depency)
+            if (is_cur_line_other_begin(line))
             {
-                fprintf(dest, "%s", depency);
-                fprintf(dest, "%s", depency_detail);
-                depency = NULL;
-                depency_detail = NULL;
-                detail_length = 0;
+                // dependencies 结束了，把最后一个 dependency 写入 dest
+                write_and_clear_dependency(dest, &cur_dependency);
             }
             fprintf(dest, "%s", line);
             continue;
@@ -73,28 +78,17 @@ int replace_hosted_to_pathed(const char *yaml_file_name, const char *path)
 
         if (prefix("  ", line) && !prefix("   ", line)) // 开始一个新的 dependency 或开始一行注释："  #"
         {
-            /// 把旧得写入到dest中
-            if (depency)
-            {
-                fprintf(dest, "%s", depency);
-                free(depency);
-                depency = NULL;
-            }
-            if(depency_detail)
-            {
-                fprintf(dest, "%s", depency_detail);
-                free(depency_detail);
-                depency_detail = NULL;
-            }
+            /// 把上一个 dependency 写入到 dest 中
+            write_and_clear_dependency(dest, &cur_dependency);
 
-            detail_length = 0;
-            depency = calloc(sizeof(char), line_length + 1);
-            memcpy(depency, line, line_length + 1);
-            is_cur_denpency_private = false;
+            cur_dependency.name = calloc(sizeof(char), line_length + 1);
+            memcpy(cur_dependency.name, line, line_length + 1);
+            cur_dependency.valid = true;
+            
         }
         else // 处理一个 dependency 的属性：depency_detail
         {
-            if (is_cur_denpency_private)
+            if (cur_dependency.private)
                 continue;
 
             // todo get and check version
@@ -102,25 +96,25 @@ int replace_hosted_to_pathed(const char *yaml_file_name, const char *path)
             if (strcmp("      url: *private_pub_server\n", line) == 0)
             {
                 /// found!
-                is_cur_denpency_private = true;
-                if (depency_detail)
-                    free(depency_detail);
-                depency_detail = calloc(sizeof(char), strlen(path) + 1);
-                memcpy(depency_detail, path, strlen(path) + 1);
+                cur_dependency.private = true;
+                if (cur_dependency.properties)
+                    free(cur_dependency.properties);
+                cur_dependency.properties = calloc(sizeof(char), strlen(path) + 1);
+                memcpy(cur_dependency.properties, path, strlen(path) + 1);
             }
             else
             {
                 // 把当前行 append 到 depency_detail 中
-                char *s = calloc(sizeof(char), detail_length + line_length + 1);
-                if (detail_length > 0)
+                char *s = calloc(sizeof(char), cur_dependency.properties_length + line_length + 1);
+                if (cur_dependency.properties_length > 0)
                 {
-                    memcpy(s, depency_detail, detail_length);
+                    memcpy(s, cur_dependency.properties, cur_dependency.properties_length);
                 }
-                memcpy(s + detail_length, line, line_length + 1);
-                if (depency_detail)
-                    free(depency_detail);
-                depency_detail = s;
-                detail_length += line_length;
+                memcpy(s + cur_dependency.properties_length, line, line_length + 1);
+                if (cur_dependency.properties_length > 0)
+                    free(cur_dependency.properties);
+                cur_dependency.properties = s;
+                cur_dependency.properties_length += line_length;
             }
         }
     }
@@ -136,12 +130,35 @@ bool prefix(const char *pre, const char *str)
     return strncmp(pre, str, strlen(pre)) == 0;
 }
 
+// 当前行是依赖声明的开始
 bool is_cur_line_dependencies_begin(const char *line)
 {
     return !strcmp(line, "dependencies:\n") || !strcmp(line, "dev_dependencies:\n");
 }
 
+// 当前行是其他声明的开始，例如："flutter_icons:"
 bool is_cur_line_other_begin(const char *line)
 {
     return !prefix(" ", line) && !prefix("\n", line) && !is_cur_line_dependencies_begin(line);
+}
+
+void write_and_clear_dependency(FILE *file, dependency *d)
+{
+    if (!d->valid)
+        return;
+    if (d->name)
+    {
+        fprintf(file, "%s", d->name);
+        free(d->name);
+    }
+    if (d->properties)
+    {
+        fprintf(file, "%s", d->properties);
+        free(d->properties);
+    }
+    d->name = NULL;
+    d->properties = NULL;
+    d->properties_length = 0;
+    d->valid = false;
+    d->private = false;
 }
